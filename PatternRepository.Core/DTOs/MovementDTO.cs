0dda7790beb2f314ad25c2a95ed377e4ced23a25
Martin Martínez <martimartinez27@hotmail.com>
2023-02-26 23:26:31 -0500

    Cargue

---
+++ PatternRepository.Core/DTOs/MovementDTO.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatternRepository.Core.DTOs
+{
+    public class MovementDTO
+    {
+        public DateTime Date { get; set; }
+        public string CustomerName { get; set; }
+        public string AccountNumber { get; set; }
+        public string TypeAccount { get; set; }
+        public decimal InitialBalance { get; set; }
+        public bool State { get; set; }
+        public decimal Value { get; set; }
+        public decimal Balance { get; set; }
+
+
+    }
+}
